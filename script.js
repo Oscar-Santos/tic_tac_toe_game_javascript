be8a27c48deeebe10beb12a43609bbe11335b998
Oscar Santos <oscar303data@gmail.com>
2023-06-28 21:32:36 -0600

    set up the logic of X's and O's, create function lineClick

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const lines = document.querySelectorAll(".line");
 const player_x = "X";
-const player_y = "Y";
+const player_o = "O";
 // tracking the turn of the player
 let turn = player_x;
 
@@ -18,7 +18,30 @@ const playagain = document.getElementById("play-again");
 const clickSound = new Audio("sounds/click.wav)")
 const gameOverSound = new Audio("sounds/game_over.wav")
 
+lines.forEach((line) => line.addEventListener("click", lineClick));
 
+function lineClick(event) {
+    if (gameOverArea.classList.contains("visible")) {
+      return;
+    }
+  
+    const line = event.target;
+    const lineNumber = line.dataset.index;
+    if (line.innerText != "") {
+      return;
+    }
+  
+    if (turn === player_x) {
+      line.innerText = player_x;
+      boardState[lineNumber - 1] = player_x;
+      turn = player_o;
+    } else {
+      line.innerText = player_o;
+      boardState[lineNumber - 1] = player_o;
+      turn = player_x;
+    }
+  
+  }
 
 
 
