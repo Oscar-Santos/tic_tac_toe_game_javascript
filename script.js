2d8820152069bdde95f09c1387222cbad8b43b06
Oscar Santos <oscar303data@gmail.com>
2023-06-28 21:04:17 -0600

    adding sounds to the game

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ let turn = player_x;
 const boardState = Array(lines.length);
 boardState.fill(null);
 
+// elements
+const strike = document.getElementById("strike");
+const gameOverArea = document.getElementById("game-over-area");
+const gameOverText = document.getElementById("game-over-text");
+const playagain = document.getElementById("play-again");
+
+// adding sounds to the game:
+const clickSound = new Audio("sounds/click.wav)")
+const gameOverSound = new Audio("sounds/game_over.wav")
+
+
+
+
+
