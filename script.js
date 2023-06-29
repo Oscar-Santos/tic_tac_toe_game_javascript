bb29a8a40b61cd4c9a0f7a8d49181f46aef13b2d
Oscar Santos <oscar303data@gmail.com>
2023-06-28 22:30:03 -0600

    create function winningCombinations, to add logic for possible winning combinations for rows, columns and diagonals

--- script.js
+++ script.js
@@ -63,5 +63,19 @@ function lineClick(event) {
     setHoverText();
   }
 
+// now creating the possible winning combinations:
 
+const winningCombinations = [
+    //rows
+    { combo: [1, 2, 3], strikeClass: "strike-row-1" },
+    { combo: [4, 5, 6], strikeClass: "strike-row-2" },
+    { combo: [7, 8, 9], strikeClass: "strike-row-3" },
+    //columns
+    { combo: [1, 4, 7], strikeClass: "strike-column-1" },
+    { combo: [2, 5, 8], strikeClass: "strike-column-2" },
+    { combo: [3, 6, 9], strikeClass: "strike-column-3" },
+    //diagonals
+    { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
+    { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
+  ];
 
